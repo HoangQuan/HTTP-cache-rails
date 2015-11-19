7a27b939c7302e9878295bf632949e09a06c1321
Quan Hoang Van (framgia) <hoang.van.quan@framgia.com>
2015-11-19 17:06:42 +0700

    change column tyle

--- db/migrate/20151116030512_create_posts.rb
+++ db/migrate/20151116030512_create_posts.rb
@@ -1,13 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.text :link
-      t.string :title
+      t.string :link
+      t.text :title
       t.integer :category_id
       t.string :permalink
 
       t.string :image
-      t.string :description
+      t.text :description
       t.text   :content
 
       t.timestamps null: false
